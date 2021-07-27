b9a776ba165f2fce6c1caa00c742efe1e72ef407
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-07-27 16:00:22 +0700

    Create TagTransformer.php

---
+++ app/Transformers/TagTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use App\Models\Tag;
+
+class TagTransformer extends TransformerAbstract
+{
+    public function transform(Tag $tag)
+    {
+        return [
+            'id' => $tag->id,
+            'title' => $tag->title,
+            'slug' => $tag->slug,
+            'content' => $tag->content,
+            'created_at' => $tag->created_at,
+            'updated_at' => $tag->updated_at,
+            'total_articles' =>$tag->articles->count()
+        ];
+    }
+}
